394a3be380a1fda1610b4a9c8a1c809f49c5999c
Praneeth Bhandaru <praneethsbhandaru@gmail.com>
2020-12-12 15:26:44 -0500

    Added Ping and Basic Exception Views

--- site/config/settings.py
+++ site/config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'config',
     'rest_framework',
     'frontend',
     'api',
@@ -77,10 +78,21 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE' : 'django_cockroachdb',
+        'NAME' : 'spaceout',
+        'USER' : 'spaceout',
+        'PASSWORD': 'spaceout',
+        'HOST' : 'localhost',
+        'PORT' : 42035,
     }
 }
 
