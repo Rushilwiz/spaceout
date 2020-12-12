9ba3c8b98eaa185b4128009612e61af9e91da1ae
Praneeth Bhandaru <praneethsbhandaru@gmail.com>
2020-12-12 15:34:20 -0500

    Set up ping

--- site/api/urls.py
+++ site/api/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.urls import path
 from .views import *
 
+
 urlpatterns = [
-    # path('', ),
+    path('ping/', PingView.as_view()),
 ]
--- site/config/settings.py
+++ site/config/settings.py
@@ -134,3 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+## Custom
+
+AUTH_USER_MODEL = 'api.User'
