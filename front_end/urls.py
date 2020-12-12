a73f5abf10a542717fed703c649d2356c3727a02
Praneeth Bhandaru <praneethsbhandaru@gmail.com>
2020-12-12 11:18:03 -0500

    Added basic routing in react

--- front_end/urls.py
+++ front_end/urls.py
@@ -1,4 +1,4 @@
-"""HackTJ2020 URL Configuration
+"""djangoProject URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -13,13 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from api import urls as api_urls
-from front_end import urls as front_end_urls
+from django.urls import path
+from .views import *
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api', include(api_urls)),
-    path('', include(front_end_urls))
+    path('', index),
 ]
